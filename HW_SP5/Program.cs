433a3fad4a3e1bcc1cc894c16751e24a29c5a59b
Влад Безедика <sttasea@mail.ru>
2023-03-08 19:42:41 +0300

    In progress

--- HW_SP5/Program.cs
+++ HW_SP5/Program.cs
@@ -10,19 +10,7 @@ namespace HW_SP5
     {
         static void Main(string[] args)
         {
-            int currentfib = 0;
-            int nexfib = 1;
-
-            Console.WriteLine(currentfib);
-            Console.WriteLine(nexfib);
-
-            for (int i = 0; i < 18; i++)
-            {
-                int NextNextfib = currentfib + nexfib;
-                Console.WriteLine(NextNextfib);
-                currentfib = nexfib;
-                nexfib = NextNextfib;
-            }
+           
         }
     }
 
