2d96ca1d380970659a70886ab806d73494f9ef71
Влад Безедика <sttasea@mail.ru>
2023-03-08 19:41:09 +0300

    DONE

--- HW_SP4/Program.cs
+++ HW_SP4/Program.cs
@@ -14,15 +14,21 @@ namespace HW_SP4
             Fibonacci(n);
             Console.ReadLine();
 
-            /* ParameterizedThreadStart FibonacciAsync = null;
+             ParameterizedThreadStart FibonacciAsync = null;
              Thread thread = new Thread(new ParameterizedThreadStart(FibonacciAsync));//поточный метод
-             thread.Start(5);*/
+             thread.Start(5);
         }
-        public static void Fibonacci(int len)
+        public static int Fibonacci(int len)
         {
             Fibonacci_Temp(0, 1, 1, len);
-
+            return len;
         }
+        //public static int Fibonacci(int len)
+        //{
+        //    if (len <= 1)
+        //        return len;
+        //    return Fibonacci(len - 1) + Fibonacci(len - 2);
+        //}
 
         private static void Fibonacci_Temp(int a, int b, int counter, int len)
         {
@@ -32,11 +38,11 @@ namespace HW_SP4
                 Fibonacci_Temp(b, a + b, counter + 1, len);
             }
         }
-       /* static void FibonacciAsync(object obj) //поточный метод
+        static  void FibonacciAsync(object obj) //поточный метод
         {
             obj = Fibonacci((int)obj);
             Console.WriteLine(((int)obj).ToString());
-        }*/
+        }
     }
 
 }
---
+++ HW_SP5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_SP5
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentfib = 0;
+            int nexfib = 1;
+
+            Console.WriteLine(currentfib);
+            Console.WriteLine(nexfib);
+
+            for (int i = 0; i < 18; i++)
+            {
+                int NextNextfib = currentfib + nexfib;
+                Console.WriteLine(NextNextfib);
+                currentfib = nexfib;
+                nexfib = NextNextfib;
+            }
+        }
+    }
+
+}
