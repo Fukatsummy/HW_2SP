aa1e6bce25b53640efee9f94c03a4405f0931ed9
Влад Безедика <sttasea@mail.ru>
2023-03-08 19:26:39 +0300

    in progress

---
+++ HW_SP4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_SP4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = 35;
+            Fibonacci(n);
+            Console.ReadLine();
+
+            /* ParameterizedThreadStart FibonacciAsync = null;
+             Thread thread = new Thread(new ParameterizedThreadStart(FibonacciAsync));//поточный метод
+             thread.Start(5);*/
+        }
+        public static void Fibonacci(int len)
+        {
+            Fibonacci_Temp(0, 1, 1, len);
+
+        }
+
+        private static void Fibonacci_Temp(int a, int b, int counter, int len)
+        {
+            if (counter <= len)
+            {
+                Console.Write("{0} ", a);
+                Fibonacci_Temp(b, a + b, counter + 1, len);
+            }
+        }
+       /* static void FibonacciAsync(object obj) //поточный метод
+        {
+            obj = Fibonacci((int)obj);
+            Console.WriteLine(((int)obj).ToString());
+        }*/
+    }
+
+}
